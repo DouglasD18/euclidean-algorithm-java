cdbbacde49276b80bdb99e720241038e4eeea308
Douglinhas <daguiaralcantara@gmail.com>
2023-03-23 18:02:40 -0300

    Class implemantation.

--- src/EuclideanAlgorithm.java
+++ src/EuclideanAlgorithm.java
@@ -1,9 +1,19 @@
 
 public class EuclideanAlgorithm {
 
+  private static int recursiveCalc(int big, int low) {
+    if (low == 0) {
+      return big;
+    }
+
+    int module = big % low;
+    return EuclideanAlgorithm.recursiveCalc(low, module);
+  }
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    int mdc = EuclideanAlgorithm.recursiveCalc(270, 192);
+    System.out.println(mdc);
   }
 
 }
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author douglas
- *
- */
-module EuclideanAlgorithm {
-}
